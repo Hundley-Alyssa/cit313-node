28798a9b72289f517a58ad8710ec9cc89b2afd1e
Alyssa Hundley <ahundley11@gmail.com>
2019-03-09 20:36:52 -0700

    sdf

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/mailView'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
